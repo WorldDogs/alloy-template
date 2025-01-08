96becc2095ef369c55c42d31bc579c333489f694
WorldDogs <33647825+WorldDogs@users.noreply.github.com>
2025-01-09 07:44:09 +0800

    add provider

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
-use alloy::{providers::{Provider, ProviderBuilder}, sol};
-
-
+use alloy::{
+    providers::{Provider, ProviderBuilder},
+    sol,
+};
 
 sol!(
     #[sol(rpc)]
     Fluid,
     "abi/abi.json"
-
 );
 
 #[tokio::main]
 async fn main() {
+    let rpc = "https://eth.llamarpc.com".parse().unwrap();
+    let provider = ProviderBuilder::new().on_http(rpc);
+    let bn = provider.get_block_number().await.unwrap();
+    println!("Current block number: {}", bn);
 }
